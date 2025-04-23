811b50f306ca1d806e84612b7b8106e0a6f91621
Bangges1288 <imbang.abdulhakim@gmail.com>
2025-04-23 11:00:54 +0700

    admin test

--- src/main/java/net/imbangabd/kelasminat/ModKelasMinat.java
+++ src/main/java/net/imbangabd/kelasminat/ModKelasMinat.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Very Important
 public class ModKelasMinat implements ModInitializer {
 	public static final String MOD_ID = "mod-kelas-minat";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
